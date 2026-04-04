4c55eb6267d78c0b02a25d4e185d6694bb24f770
Thorndillo <maahim.saif@education.nsw.gov.au>
2026-04-05 01:10:49 +1100

    Updated for direction locking
    Implemented direction locking so the script will prioritise you moving in whatever direction you're currently going. Feels like i added a large amount of script, so please review code.

--- Scripts/animatable_body_2d.gd
+++ Scripts/animatable_body_2d.gd
@@ -3,6 +3,10 @@ extends AnimatableBody2D
 #And I want to make it a quick edit from editor using @export
 @export var speed = 180
 var Direction: Vector2
+var Priority1 = "Down"
+var Priority2 = "Up"
+var Priority3 = "Left"
+var Priority4 = "Right"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,12 +19,58 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	Direction.x = 0
 	Direction.y = 0
-	if Input.is_action_pressed("Down"):
-		Direction.y = 1
-	elif Input.is_action_pressed("Up"):
+	if Input.is_action_pressed(Priority1):
+		if Priority1 == "Left":
+			Direction.x = -1
+		if Priority1 == "Right":
+			Direction.x = 1
+		if Priority1 == "Down":
+			Direction.y = 1
+		if Priority1 == "Up":
+			Direction.y = -1
+	elif Input.is_action_pressed(Priority2):
 		Direction.y = -1
-	elif Input.is_action_pressed("Left"):
+		var holdingvariable = Priority1
+		Priority1 = Priority2
+		Priority2 = Priority3
+		Priority3 = Priority4
+		Priority4 = holdingvariable
+		
+		if Priority1 == "Left":
+			Direction.x = -1
+		if Priority1 == "Right":
+			Direction.x = 1
+		if Priority1 == "Down":
+			Direction.y = 1
+		if Priority1 == "Up":
+			Direction.y = -1
+	elif Input.is_action_pressed(Priority3):
 		Direction.x = -1
-	elif Input.is_action_pressed("Right"):
+		var holdingvariable = Priority1
+		Priority1 = Priority3
+		Priority3 = Priority4
+		Priority4 = holdingvariable
+		
+		if Priority1 == "Left":
+			Direction.x = -1
+		if Priority1 == "Right":
+			Direction.x = 1
+		if Priority1 == "Down":
+			Direction.y = 1
+		if Priority1 == "Up":
+			Direction.y = -1
+	elif Input.is_action_pressed(Priority4):
 		Direction.x = 1
+		var holdingvariable = Priority1
+		Priority1 = Priority4
+		Priority4 = holdingvariable
+		
+		if Priority1 == "Left":
+			Direction.x = -1
+		if Priority1 == "Right":
+			Direction.x = 1
+		if Priority1 == "Down":
+			Direction.y = 1
+		if Priority1 == "Up":
+			Direction.y = -1
 	$".".position += speed * delta * Direction
