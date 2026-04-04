00dc1b630748f195b08879e54a3dc73ebfeba997
Thorndillo <maahim.saif@education.nsw.gov.au>
2026-04-05 00:50:30 +1100

    Made movement more efficient with diagonal prevention
    I made the movement more efficient using the method engulfed showed me and have diagonal prevention, though planning to improve later.

--- Scripts/animatable_body_2d.gd
+++ Scripts/animatable_body_2d.gd
@@ -1,9 +1,8 @@
 extends AnimatableBody2D
-
-var goingdown := false
-var goingup := false
-var goingleft := false
-var goingright := false
+# I know speed should be a const, but if we don't add anything to change it in the code it's fine.
+#And I want to make it a quick edit from editor using @export
+@export var speed = 180
+var Direction: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,31 +10,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#if diagonal movement is implemented make it 20.5 for both axes instead of 29 for both axes
-	if goingdown:
-		$".".position += Vector2(0,29)
-	if goingup:
-		$".".position -= Vector2(0,29)
-	if goingleft:
-		$".".position -= Vector2(29,0)
-	if goingright:
-		$".".position += Vector2(29,0)
-func _input(event: InputEvent) -> void:
-	if Input.is_action_just_released("Down"):
-		goingdown = false
-	if Input.is_action_just_released("Up"):
-		goingup = false
-	if Input.is_action_just_released("Right"):
-		goingright = false
-	if Input.is_action_just_released("Left"):
-		goingleft = false
-	if not goingup or goingdown or goingleft or goingright:
-		if Input.is_action_just_pressed("Up"):
-			goingup = true
-		if Input.is_action_just_pressed("Down"):
-			goingdown = true
-		if Input.is_action_just_pressed("Left"):
-			goingleft = true
-		if Input.is_action_just_pressed("Right"):
-			goingright = true
-	
+	pass
+
+func _physics_process(delta: float) -> void:
+	Direction.x = 0
+	Direction.y = 0
+	if Input.is_action_pressed("Down"):
+		Direction.y = 1
+	elif Input.is_action_pressed("Up"):
+		Direction.y = -1
+	elif Input.is_action_pressed("Left"):
+		Direction.x = -1
+	elif Input.is_action_pressed("Right"):
+		Direction.x = 1
+	$".".position += speed * delta * Direction
