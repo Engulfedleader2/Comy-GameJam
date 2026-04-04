687aa29c25de3bc4914c075cd2ecbcfbf018f91b
Thorndillo <maahim.saif@education.nsw.gov.au>
2026-04-05 01:21:30 +1100

    Update animatable_body_2d.gd

--- Scripts/animatable_body_2d.gd
+++ Scripts/animatable_body_2d.gd
@@ -16,61 +16,40 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func prioritychecker(Priority):
+	if Priority1 == "Left":
+		Direction.x = -1
+	if Priority1 == "Right":
+		Direction.x = 1
+	if Priority1 == "Down":
+		Direction.y = 1
+	if Priority1 == "Up":
+		Direction.y = -1
+
 func _physics_process(delta: float) -> void:
 	Direction.x = 0
 	Direction.y = 0
 	if Input.is_action_pressed(Priority1):
-		if Priority1 == "Left":
-			Direction.x = -1
-		if Priority1 == "Right":
-			Direction.x = 1
-		if Priority1 == "Down":
-			Direction.y = 1
-		if Priority1 == "Up":
-			Direction.y = -1
+		prioritychecker(Priority1)
 	elif Input.is_action_pressed(Priority2):
-		Direction.y = -1
 		var holdingvariable = Priority1
 		Priority1 = Priority2
 		Priority2 = Priority3
 		Priority3 = Priority4
 		Priority4 = holdingvariable
 		
-		if Priority1 == "Left":
-			Direction.x = -1
-		if Priority1 == "Right":
-			Direction.x = 1
-		if Priority1 == "Down":
-			Direction.y = 1
-		if Priority1 == "Up":
-			Direction.y = -1
+		prioritychecker(Priority1)
 	elif Input.is_action_pressed(Priority3):
-		Direction.x = -1
 		var holdingvariable = Priority1
 		Priority1 = Priority3
 		Priority3 = Priority4
 		Priority4 = holdingvariable
 		
-		if Priority1 == "Left":
-			Direction.x = -1
-		if Priority1 == "Right":
-			Direction.x = 1
-		if Priority1 == "Down":
-			Direction.y = 1
-		if Priority1 == "Up":
-			Direction.y = -1
-	elif Input.is_action_pressed(Priority4):
-		Direction.x = 1
+		prioritychecker(Priority1)
+	elif Input.is_action_pressed(Priority4):	
 		var holdingvariable = Priority1
 		Priority1 = Priority4
 		Priority4 = holdingvariable
 		
-		if Priority1 == "Left":
-			Direction.x = -1
-		if Priority1 == "Right":
-			Direction.x = 1
-		if Priority1 == "Down":
-			Direction.y = 1
-		if Priority1 == "Up":
-			Direction.y = -1
+		prioritychecker(Priority1)
 	$".".position += speed * delta * Direction
